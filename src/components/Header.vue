2bc91b749c6d09d042a1f8442d604615b9d90dbe
Nestor Ramirez <nesjesramrey@gmail.com>
2025-03-30 17:54:42 -0600

    add icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,10 @@
 <template>
   <header class="header">
     <nav class="nav-container">
+      <div class="container-logo">
+      <img src="../assets/iconjatech.png" alt="Perfil" class="hero-icon" />
       <div class="logo">Jatech</div>
+    </div>
       <div class="nav-links">
         <router-link to="/" class="nav-link">Inicio</router-link>
         <router-link to="/portfolio" class="nav-link">Portafolio</router-link>
@@ -30,18 +33,38 @@ export default {
   z-index: 1000;
 }
 
+
+
 .nav-container {
   max-width: 1200px;
   margin: 0 auto;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
+}
+
+.container-logo{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.hero-icon {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin: auto 1.5rem;
+  display: block;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 .logo {
   font-size: 1.5rem;
   font-weight: bold;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 
 .nav-links {
